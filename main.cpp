d15234d4ad47c6636f798cd9a2bf6407aaf4be7b
Koy Wilson <koy.m.wilson-1@ou.edu>
2022-04-01 11:12:41 -0500

    Adding initial commit

--- main.cpp
+++ main.cpp
@@ -1,52 +1,12 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
-void change(int* arr, int size)
+class String
 {
+    
+};
 
-    for(int index = 0; index < size; ++index)
-    {
-        arr[index] = 20;
-    }
-}
-
-void doubleValue(int& value)
-{
-    value = value + value;
-}
-
-void display(int* arr, int size)
-{
-    for(int index = 0; index < size; ++index)
-    {
-        cout << arr[index] << endl;
-    }
-}
-
-// Driver Code
-int main()
-{
-    int* test;
-    int arr[] = {1, 2, 3, 4, 5};
-
-    test = arr;
-
-    display(test, 5);
-
-    change(arr, 5);
-
-    test = arr;
-
-    display(test, 5);
-
-    int testing = 5;
-
-    cout << testing << endl;
-
-    doubleValue(testing);
-
-    cout << testing << endl;
-
+int main() {
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
